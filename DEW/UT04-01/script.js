3e6f3c1a004ca74df30abe0cbfee58058d864160
mensaro <mensaro@gmail.com>
2024-11-28 09:58:23 +0000

    Initial commit

---
+++ DEW/UT04-01/script.js
@@ -0,0 +1,15 @@
+function updateCounter(fieldId, maxLength) {
+    const field = document.getElementById(fieldId);
+    const counter = document.getElementById(`${fieldId}-counter`);
+    const currentLength = field.value.length;
+    counter.textContent = `${currentLength} / ${maxLength}`;
+}
+
+function togglePasswordVisibility() {
+    const passwordField = document.getElementById('password');
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+    }
+}
